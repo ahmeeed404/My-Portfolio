d53ae469b1d2aa960dfe98843da475b6d0e8f558
ahmeeed404 <ahmed.achoukhan04@gmail.com>
2026-04-02 18:53:24 +0200

    fix: correct vite base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
+  base: '/My-https://github.com/ahmeeed404/My-Portfolio.git/' // ⚠️ Set to your repo name for GitHub Pages
 })

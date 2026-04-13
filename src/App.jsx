681e43b1e4200028e4dbf53c38b39b337be2925f
ahmeeed404 <ahmed.achoukhan04@gmail.com>
2026-04-13 16:37:04 +0200

    chore: prepare for vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from './components/footer'
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#050816] text-slate-200">
+    <div className="min-h-screen">
       <Navbar />
       <main>
         <Hero />
